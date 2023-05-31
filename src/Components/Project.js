5965f03ffc757f5f141a840ac56439a68860a131
Carolina Teselman <100797700+carotesel@users.noreply.github.com>
2023-05-31 14:06:16 -0300

    fix mb and project cards

--- src/Components/Project.js
+++ src/Components/Project.js
@@ -2,13 +2,16 @@ import React from "react";
 
 function Project({ title, image, description, techstack, previewLink, githubLink }) {
   return (
-    <article className="rounded-xl mt-10 overflow-hidden shadow-xl shadow-slate-300 dark:shadow-slate-900">
+    <a        href={previewLink}
+              target="_blank"
+              rel="noreferrer noopener">
+    <article className="rounded-xl mt-10 overflow-hidden shadow-xl shadow-slate-300 dark:shadow-slate-900 md:hover:shadow-2xl md:hover:shadow-slate-400">
       <img src={image} alt="" loading="lazy" />
       <div className="dark:bg-dark-card p-4">
         <h1 className="dark:text-light-heading font-semibold text-lg pt-1">{title}</h1>
         <p className="text-content pt-4 font-light">{description}</p>
         <h3 className="text-dark-heading dark:text-light-heading font-medium pt-4">
-          Tech Stack : <span className="font-light">{techstack}</span>
+          Tech Stack: <span className="font-light">{techstack}</span>
         </h3>
         <div className="flex justify-between items-center mt-8">
           <div className="flex items-center">
@@ -70,6 +73,7 @@ function Project({ title, image, description, techstack, previewLink, githubLink
         </div>
       </div>
     </article>
+    </a>
   );
 }
 
